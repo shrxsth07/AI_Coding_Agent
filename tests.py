033ff40e26c87b8a945602369d4e60fef8dfafb5
Shresth <shrxsth16@gmail.com>
2026-09-02 20:56:10 +0530

    get_file_content.py file made

--- tests.py
+++ tests.py
@@ -1,16 +1,20 @@
 from Functions.get_files_info import get_files_info
+from Functions.get_file_content import get_file_content
 
 
 def main():
     working_dir = "calculator"
+    # root_contents = get_files_info(working_dir)
+    # print(root_contents)
+    # pkg_contents = get_files_info(working_dir, "pkg")
+    # print(pkg_contents)
+    # pkg_contents = get_files_info(working_dir, "/bin")
+    # print(pkg_contents)
+    # pkg_contents = get_files_info(working_dir, "../")
+    # print(pkg_contents)
 
-    root_contents = get_files_info(working_dir)
-    print(root_contents)
-    pkg_contents = get_files_info(working_dir, "pkg")
-    print(pkg_contents)
-    pkg_contents = get_files_info(working_dir, "/bin")
-    print(pkg_contents)
-    pkg_contents = get_files_info(working_dir, "../")
-    print(pkg_contents)
+    print(get_file_content(working_dir, "main.py"))
+    print(get_file_content(working_dir, "pkg/calculator.py"))
+    print(get_file_content(working_dir, "/bin/cat"))
 
 main()
