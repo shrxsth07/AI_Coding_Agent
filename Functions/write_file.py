b301caf21444ea2f5abc1ad90e2fdc74c1ea2028
Shresth <shrxsth16@gmail.com>
2026-09-02 22:03:31 +0530

    write_file.py file made

---
+++ Functions/write_file.py
@@ -0,0 +1,24 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: "{file_path}" is not in the working directory'
+
+    parent_dir = os.path.dirname(abs_file_path)
+    if not os.path.isdir(parent_dir):
+        try:
+            os.makedirs(parent_dir)
+        except Exception as e:
+            return f"Could not create parent dirs: {parent_dir} = {e}"    
+
+    try:
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+        return (
+            f'Successfully wrote to "{file_path}" ({len(content)} characters)'
+        )
+
+    except Exception as e:
+        return f"Failed to write to file: {file_path}, {e}"
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 from Functions.get_files_info import get_files_info
 from Functions.get_file_content import get_file_content
-
+from Functions.write_file import write_file
 
 def main():
     working_dir = "calculator"
@@ -13,8 +13,10 @@ def main():
     # pkg_contents = get_files_info(working_dir, "../")
     # print(pkg_contents)
 
-    print(get_file_content(working_dir, "main.py"))
-    print(get_file_content(working_dir, "pkg/calculator.py"))
-    print(get_file_content(working_dir, "/bin/cat"))
+    # print(get_file_content(working_dir, "main.py"))
+    # print(get_file_content(working_dir, "pkg/calculator.py"))
+    # print(get_file_content(working_dir, "/bin/cat"))\
+
+    print(write_file(working_dir, "pkg/morelorem.txt", "testing to create a new file"))
 
 main()
